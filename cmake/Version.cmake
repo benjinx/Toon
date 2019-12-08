2197da98d04ec7128eec38d8fdb2b669d9f74ca9
Stephen Lane-Walsh <sdl.slane@gmail.com>
2019-12-08 01:00:44 -0500

    Chore(Build): Fix DLLs and cleanup (#24)
    * Chore(Build): Fix DLLs and cleanup
    
    Move version code into Version.cmake
    Move packaging code into packages/CMakeLists.txt
    Cleanup main CMakeLists.txt
    Query DLL path from interface libraries
    
    * Disable assimp's "d" suffix for libraries
    
    * Fix PATH variables in demos
    Fix glfw3 thirdparty
    
    * Upgrade Travis to use Ubuntu 18.04

---
+++ cmake/Version.cmake
@@ -0,0 +1,34 @@
+
+INCLUDE(GetGitRevisionDescription)
+
+# Get Git revision
+GET_GIT_HEAD_REVISION(GIT_REFSPEC PROJECT_VERSION_GIT_HASH)
+STRING(SUBSTRING "${PROJECT_VERSION_GIT_HASH}" 0 8 PROJECT_VERSION_GIT_HASH)
+
+GIT_DESCRIBE(VERSION --tags)
+IF(VERSION)
+    STRING(REGEX REPLACE 
+        "^v([0-9]+)\\..*" "\\1" 
+        PROJECT_VERSION_MAJOR 
+        "${VERSION}"
+    )
+    STRING(REGEX REPLACE 
+        "^v[0-9]+\\.([0-9]+).*" "\\1" 
+        PROJECT_VERSION_MINOR 
+        "${VERSION}"
+    )
+    STRING(REGEX REPLACE 
+        "^v[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" 
+        PROJECT_VERSION_PATCH 
+        "${VERSION}"
+    )
+    SET(PROJECT_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
+
+    MESSAGE(STATUS "Temporality Version ${PROJECT_VERSION}")
+ELSE()
+    SET(PROJECT_VERSION_MAJOR 0)
+    SET(PROJECT_VERSION_MINOR 0)
+    SET(PROJECT_VERSION_PATCH 0)
+    
+    MESSAGE(WARNING "Failed to parse version from `git describe --tags`")
+ENDIF()
